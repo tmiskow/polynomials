dee1bbbd3b1df1a6b4dda8cebd995e03c93f039b
Tomasz Miśków <tm385898@students.mimuw.edu.pl>
2017-05-04 17:11:08 +0200

    Implementacja PolyClone, MonoClone i MonoListClone

--- src/poly.c
+++ src/poly.c
@@ -23,9 +23,50 @@ void MonoListDestroy(Mono *m)
 	}
 }
 
+/**
+ * Robi pełną, głęboką kopię wszystkich jednomianów w liście.
+ * @param[in] m : pierwszy jednomian w liście
+ * @return sklonowana lista jednomianów
+ */
+Mono* MonoListClone(const Mono *m)
+{
+	if (m)
+	{
+	  	Mono* mono_list_clone = (Mono*) malloc(sizeof (Mono));
+		assert(mono_list_clone);
+		*mono_list_clone = MonoClone(m);
+		return mono_list_clone;
+	}
+
+	else
+	{
+		return NULL;
+	}
+}
+
+
 // Funkcje główne
 
 void PolyDestroy(Poly *p)
 {
 	MonoListDestroy(p->mono_list);
+}
+
+Poly PolyClone(const Poly *p)
+{
+	return (Poly)
+	{
+		.mono_list = MonoListClone(p->mono_list),
+		.coeff = p->coeff
+	};
+}
+
+Mono MonoClone(const Mono *m)
+{
+	return (Mono)
+	{
+		.p = PolyClone(&(m->p)),
+		.exp = m->exp,
+		.next = MonoListClone(m->next)
+	};
 }
