48004b074bb938f22f37c260462413809222d49d
Tomasz Miśków <tm385898@students.mimuw.edu.pl>
2017-05-08 19:16:50 +0200

    Dodane placeholdery niezaimplementowanych funkcji z poly.h

---
+++ src/poly.c
@@ -0,0 +1,79 @@
+#include "poly.h"
+
+// Główne funkcje biblioteki
+
+void PolyDestroy(Poly *p)
+{
+	/* TODO */
+	assert(false);
+}
+
+Poly PolyClone(const Poly *p)
+{
+	/* TODO */
+	assert(false);
+	return PolyZero();
+}
+
+Poly PolyAdd(const Poly *p, const Poly *q)
+{
+	/* TODO */
+	assert(false);
+	return PolyZero();
+}
+
+Poly PolyAddMonos(unsigned count, const Mono monos[])
+{
+	/* TODO */
+	assert(false);
+	return PolyZero();
+}
+
+Poly PolyMul(const Poly *p, const Poly *q)
+{
+	/* TODO */
+	assert(false);
+	return PolyZero();
+}
+
+Poly PolyNeg(const Poly *p)
+{
+	/* TODO */
+	assert(false);
+	return PolyZero();
+}
+
+Poly PolySub(const Poly *p, const Poly *q)
+{
+	/* TODO */
+	assert(false);
+	return PolyZero();
+}
+
+poly_exp_t PolyDegBy(const Poly *p, unsigned var_idx)
+{
+	/* TODO */
+	assert(false);
+	return 0;
+}
+
+poly_exp_t PolyDeg(const Poly *p)
+{
+	/* TODO */
+	assert(false);
+	return 0;
+}
+
+bool PolyIsEq(const Poly *p, const Poly *q)
+{
+	/* TODO */
+	assert(false);
+	return 0;
+}
+
+Poly PolyAt(const Poly *p, poly_coeff_t x)
+{
+	/* TODO */
+	assert(false);
+	return PolyZero();
+}
