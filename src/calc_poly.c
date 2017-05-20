0063bdeb7c9569cd656c2c256fd955e830792469
Tomasz Miśków <tm385898@students.mimuw.edu.pl>
2017-05-20 20:00:21 +0200

    Rozpoczęcie pracy nad kalkulatorem. Stworzenie pliku calc_poly.c

---
+++ src/calc_poly.c
@@ -0,0 +1,16 @@
+/** @file
+   TODO
+
+   @author Tomasz Miśków <tm385898@students.mimuw.edu.pl>
+   @copyright Uniwersytet Warszawski
+   @date TODO
+*/
+
+#include "poly.h"
+
+int int main(int argc, char const *argv[]) {
+
+	// TODO
+
+	return 0;
+}
